784548bff0da25e23d1ad69b37d04ba98d186517
Sebastian Pettersson <sebastian.pettersson@live.com>
2020-12-06 13:25:53 +0100

    simplify 06

--- challenges/06/index.js
+++ challenges/06/index.js
@@ -1,6 +1,6 @@
 const { EOL } = require('os');
 
-const countUniqueChars = str => {
+const countUniqueChars = (str) => {
   const seen = [];
   for (let char of str) {
     if (seen.indexOf(char) === -1) seen.push(char);
@@ -9,36 +9,35 @@ const countUniqueChars = str => {
   return seen.length;
 };
 
-const countExistsInAllArrays = arrays => {
+const countExistsInAllArrays = (arrays) => {
   const [array1, ...rest] = arrays;
 
   let countExistsInAll = 0;
   for (let item of array1) {
-    if (rest.every(x => x.indexOf(item) !== -1)) {
-      countExistsInAll++
+    if (rest.every((x) => x.indexOf(item) !== -1)) {
+      countExistsInAll++;
     }
   }
 
   return countExistsInAll;
-}
+};
 
 const solve1 = (groups) => {
-  let totalCount = 0;
-  for (let group of groups) {
-    const cleanedUpString = group
-      .replace(/\n/g, '') // throw away newlines
+  const totalCount = groups.reduce((totalCount, group) => {
+    const cleanedUpString = group.replace(/\n/g, ''); // throw away newlines
 
-    totalCount += countUniqueChars(cleanedUpString);
-  }
+    return totalCount + countUniqueChars(cleanedUpString);
+  }, 0);
 
   return totalCount;
 };
 
 const solve2 = (groups) => {
-  const total = groups.reduce((total, group) => {
-    return total += countExistsInAllArrays(group.split(EOL));
+  const totalCount = groups.reduce((total, group) => {
+    return (total += countExistsInAllArrays(group.split(EOL)));
   }, 0);
-  return total;
+  
+  return totalCount;
 };
 
 module.exports = {
